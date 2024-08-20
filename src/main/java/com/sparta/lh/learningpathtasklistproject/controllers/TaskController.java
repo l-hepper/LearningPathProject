de490381fd7f18912694951631f68c3c04c36440
Liam <33605657+l-hepper@users.noreply.github.com>
2024-08-20 08:35:52 +0100

    adds TaskController and CRUD methods

---
+++ src/main/java/com/sparta/lh/learningpathtasklistproject/controllers/TaskController.java
@@ -0,0 +1,71 @@
+package com.sparta.lh.learningpathtasklistproject.controllers;
+
+import com.sparta.lh.learningpathtasklistproject.entities.Task;
+import com.sparta.lh.learningpathtasklistproject.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    TaskRepository taskRepository;
+
+    @Autowired
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Task>> getAllTasks() {
+        List<Task> tasks = taskRepository.findAll();
+        return ResponseEntity.ok(tasks);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable int id) {
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            return ResponseEntity.ok(task.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Task> addTask(@RequestBody Task task) {
+        Task savedTask = taskRepository.save(task);
+        URI location = URI.create("/tasks/" + savedTask.getId());
+        return ResponseEntity.created(location).body(savedTask);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable int id, @RequestBody Task task) {
+        Optional<Task> existingTaskOptional = taskRepository.findById(id);
+        if (existingTaskOptional.isPresent()) {
+            Task existingTask = existingTaskOptional.get();
+            existingTask.setName(task.getName());
+            existingTask.setDeadline(task.getDeadline());
+            existingTask.setUsers(task.getUsers());
+            Task updatedTask = taskRepository.save(existingTask);
+            return ResponseEntity.ok(updatedTask);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable int id) {
+        if (taskRepository.existsById(id)) {
+            taskRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
