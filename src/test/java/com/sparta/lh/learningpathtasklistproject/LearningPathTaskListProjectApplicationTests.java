fd51d10a2b9021e64de15b694e54f01f78bfe3b3
Liam <33605657+l-hepper@users.noreply.github.com>
2024-08-20 07:34:11 +0100

    initial commit

---
+++ src/test/java/com/sparta/lh/learningpathtasklistproject/LearningPathTaskListProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.lh.learningpathtasklistproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningPathTaskListProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
