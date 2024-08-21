ea595ff4bbf97125baacc646ec3903e82af4b58a
Liam <33605657+l-hepper@users.noreply.github.com>
2024-08-21 09:16:34 +0100

    adds hateaos to methods in UserController

--- src/main/java/com/sparta/lh/learningpathtasklistproject/controllers/UserController.java
+++ src/main/java/com/sparta/lh/learningpathtasklistproject/controllers/UserController.java
@@ -7,6 +7,7 @@ import com.sparta.lh.learningpathtasklistproject.repositories.TaskRepository;
 import com.sparta.lh.learningpathtasklistproject.repositories.UserRepository;
 import com.sparta.lh.learningpathtasklistproject.repositories.UserTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.Link;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -38,9 +39,19 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers() {
+    public ResponseEntity<List<EntityModel<User>>> getAllUsers() {
         List<User> users = userRepository.findAll();
-        return ResponseEntity.ok(users);
+
+        // Convert each User to an EntityModel with HATEOAS links
+        List<EntityModel<User>> userModels = users.stream()
+                .map(user -> {
+                    Link selfLink = linkTo(methodOn(this.getClass()).getUserById(user.getId())).withSelfRel();
+                    Link tasksLink = linkTo(methodOn(UserController.class).getUserTasks(user.getId())).withRel("tasks");
+                    return EntityModel.of(user, selfLink, tasksLink);
+                })
+                .toList();
+
+        return ResponseEntity.ok(userModels);
     }
 
     @GetMapping("/{id}")
@@ -48,13 +59,9 @@ public class UserController {
         Optional<User> user = userRepository.findById(id);
         if (user.isPresent()) {
             User userEntity = user.get();
-
             Link selfLink = linkTo(methodOn(this.getClass()).getUserById(id)).withSelfRel();
-
             Link tasksLink = linkTo(methodOn(UserController.class).getUserTasks(id)).withRel("tasks");
-
             EntityModel<User> resource = EntityModel.of(userEntity, selfLink, tasksLink);
-
             return ResponseEntity.ok(resource);
         } else {
             return ResponseEntity.notFound().build();
@@ -62,21 +69,30 @@ public class UserController {
     }
 
     @GetMapping("/{userId}/tasks")
-    public ResponseEntity<List<Task>> getUserTasks(@PathVariable Integer userId) {
+    public ResponseEntity<List<EntityModel<Task>>> getUserTasks(@PathVariable Integer userId) {
         List<UserTask> userTasks = userTaskRepository.findUserTasksByUserId(userId);
-        List<Task> tasks = userTasks.stream().map(UserTask::getTask).collect(Collectors.toList());
-        return ResponseEntity.ok(tasks);
+        List<EntityModel<Task>> taskModels = userTasks.stream()
+                .map(userTask -> {
+                    Task task = userTask.getTask();
+                    Link selfLink = linkTo(methodOn(TaskController.class).getTaskById(task.getId())).withSelfRel();
+                    Link linkToUsers = linkTo(methodOn(TaskController.class).getUsersByTask(task.getId())).withRel("users");
+                    return EntityModel.of(task, selfLink, linkToUsers);
+                })
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(taskModels);
     }
 
     @PostMapping
-    public ResponseEntity<User> addUser(@RequestBody User user) {
+    public ResponseEntity<EntityModel<User>> addUser(@RequestBody User user) {
         User savedUser = userRepository.save(user);
-        URI location = URI.create("/users/" + savedUser.getId());
-        return ResponseEntity.created(location).body(savedUser);
+        Link selfLink = linkTo(methodOn(UserController.class).getUserById(savedUser.getId())).withSelfRel();
+        Link tasksLink = linkTo(methodOn(UserController.class).getUserTasks(savedUser.getId())).withRel("tasks");
+        EntityModel<User> resource = EntityModel.of(savedUser, selfLink, tasksLink);
+        return ResponseEntity.created(selfLink.toUri()).body(resource);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable int id, @RequestBody User user) {
+    public ResponseEntity<EntityModel<User>> updateUser(@PathVariable int id, @RequestBody User user) {
         Optional<User> existingUserOptional = userRepository.findById(id);
         if (existingUserOptional.isPresent()) {
             User existingUser = existingUserOptional.get();
@@ -84,7 +100,12 @@ public class UserController {
             existingUser.setEmail(user.getEmail());
             existingUser.setPassword(user.getPassword());
             User updatedUser = userRepository.save(existingUser);
-            return ResponseEntity.ok(updatedUser);
+
+            Link selfLink = linkTo(methodOn(UserController.class).getUserById(updatedUser.getId())).withSelfRel();
+            Link tasksLink = linkTo(methodOn(UserController.class).getUserTasks(updatedUser.getId())).withRel("tasks");
+            EntityModel<User> resource = EntityModel.of(updatedUser, selfLink, tasksLink);
+
+            return ResponseEntity.ok(resource);
         } else {
             return ResponseEntity.notFound().build();
         }
